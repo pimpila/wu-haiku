896ce3c36c4eeb8b6d9a6509b37c25a736a68717
Pimpila <pimpila@gmail.com>
2017-04-23 17:47:18 -0400

    first commit

---
+++ terminals.js
@@ -0,0 +1,107 @@
+'use strict'
+
+/* global txt */
+
+// create a dictionary (nested obj) of words cataloged by syllable and part of speech:
+const terminals = {}
+
+txt.forEach( word => {
+  const numSyll = RiTa.getSyllables(word).split('/').length;
+  const posFull = RiTa.getPosTags(word);
+  let pos;
+
+  // if there is no pos identified OR the first letter doesn't match one of the pos categories,
+  // return out of the iteration:
+  if (!posFull[0] || posFull[0][0].match(/[^vwjncdpri]/)) {
+    return
+  }
+  // else set the value of pos var:
+  switch (word) {
+    case 'a':
+    pos = 'det-s';
+    break;
+    case 'each':
+    pos = 'det-s';
+    break;
+    case 'this':
+    pos = 'det-s';
+    break;
+    case 'all':
+    pos = 'det-p';
+    break;
+    case 'those':
+    pos = 'det-p';
+    break;
+    case 'these':
+    pos = 'det-p';
+    break;
+    case 'some':
+    pos = 'det-p';
+    break;
+    case 'the':
+    pos = 'det';
+    break;
+    case 'no':
+    pos = 'det';
+    break;
+  }
+
+  switch(posFull[0]) {
+    case 'nns':
+    pos = 'noun-p';
+    break;
+    case 'nn':
+    pos = 'noun-s';
+    break;
+    case 'nnp':
+    pos = 'noun-s';
+    break;
+    case 'nnps':
+    pos = 'noun-s';
+    break;
+  }
+
+  switch (posFull[0][0]) {
+    case 'v':
+      pos = 'verb';
+      break;
+    case 'i':
+      pos = 'prep';
+      break;
+    case 'w':
+      pos = 'quest';
+      break;
+    case 'j':
+      pos = 'adj';
+      break;
+    case 'c':
+      pos = 'number';
+      break;
+    case 'p':
+      pos = 'pron';
+      break;
+    case 'r':
+      pos = 'adv';
+      break;
+  }
+
+  // and add values to the dictionary
+  if (terminals[numSyll]) {
+    if (terminals[numSyll][pos]) {
+      if (terminals[numSyll][pos].indexOf(word) === -1) {
+        terminals[numSyll][pos].push(word);
+      }
+      else {
+        return
+      }
+    }
+    else {
+      terminals[numSyll][pos] = [word]
+    }
+  }
+  else {
+    terminals[numSyll] = {};
+    terminals[numSyll][pos] = [word]
+  }
+})
+console.log('terminals', terminals)
---
+++ wu-tang.js
@@ -0,0 +1,45 @@
+/* global terminals */
+
+// call new instance of RiGrammar();
+const rg = new RiGrammar();
+
+// production rules:
+
+// start/s:
+rg.addRule('<start>', ' <5-PP> \n <7-NP> \n <5-VP> ')
+rg.addRule('<start>', ' <5-NP> \n <7-VP> \n <5-PP> ')
+
+rg.addRule('<5-PP>', '<1-prep> <1-det-s> <2-adj> <1-noun-s> | <1-prep> <1-det-p> <2-adj> <1-noun-p> | <1-prep> <1-det> <2-adj> <1-noun-s> | <1-prep> <1-det> <2-adj> <1-noun-p>| <2-prep> <1-det-s> <2-noun-s> | <2-prep> <1-det-p> <2-noun-p> | <2-prep> <1-adj> <2-noun-s> | <2-prep> <1-adj> <2-noun-p> | <2-prep> <3-noun-p> | <2-prep> <3-noun-s>')
+rg.addRule('<5-VP>', '<3-verb> <2-adv> | <2-verb> <3-adv>')
+rg.addRule('<5-NP>', '<1-det-s> <1-adj> <3-noun-s> | <3-adj> <2-noun-p> | <3-adj> <2-noun-s> | <1-det-p> <1-adj> <3-noun-p> | <1-det> <1-adj> <3-noun-p> | <1-det> <1-adj> <3-noun-p> ')
+
+rg.addRule('<7-NP>', '<1-det-s> <1-adj> <5-noun-s> | <1-det-p> <1-adj> <5-noun-s> | <1-det> <1-adj> <5-noun-s> | <1-quest> <2-adj> <4-noun-p> | <1-quest> <2-adj> <4-noun-s>')
+rg.addRule('<7-VP>', '<3-verb> <4-adv> | <4-verb> <3-adv>')
+
+// for each terminal:
+
+// collect types of syllable / pos combinations:
+const types = []
+
+for (var key in terminals) {
+  for (var key2 in terminals[key]) {
+    types.push(`${key}-${key2}`)
+  }
+}
+
+// then add a rule for each terminal
+types.forEach(type => {
+  const num = type[0];
+  const pos = type.slice(2, type.length)
+  // console.log('rule: ', `<${type}>`, terminals[num][pos].join(' | '))
+  rg.addRule(`<${type}>`,  terminals[num][pos].join(' | '))
+})
+
+// expand grammar:
+const result = rg.expand();
+
+
+
+
+
+
